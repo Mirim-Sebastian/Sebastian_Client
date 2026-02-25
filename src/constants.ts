d0ff89feed471a42716fcfb1621d40e2b7223fef
hjzhhhj <jhj090120@gmail.com>
2026-02-25 14:32:59 +0900

    [Chore] :: 앱 전역 상수 정의

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+export const COLORS = [
+  { name: 'Ice', value: '#e6f0ff' },
+  { name: 'Mint', value: '#38d9a9' },
+  { name: 'Coral', value: '#ff6b5a' },
+  { name: 'Amber', value: '#ffb347' },
+  { name: 'Lime', value: '#7acb6e' },
+  { name: 'Blue', value: '#4aa3ff' },
+  { name: 'Rose', value: '#ff7ab6' },
+]
+
+export const MIN_NAME = 2
+export const MAX_NAME = 15
+export const CANVAS_BG = '#041126'
