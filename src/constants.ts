86e3a2bf17a7bedd68617bf18b3d8934fd713aba
hjzhhhj <jhj090120@gmail.com>
2026-03-02 10:13:38 +0900

    [Feat] :: 컬러팔레트 색상  추가

--- src/constants.ts
+++ src/constants.ts
@@ -6,6 +6,22 @@ export const COLORS = [
   { name: 'Lime', value: '#7acb6e' },
   { name: 'Blue', value: '#4aa3ff' },
   { name: 'Rose', value: '#ff7ab6' },
+  { name: 'Violet', value: '#7b6cff' },
+  { name: 'Sky', value: '#6bd4ff' },
+  { name: 'Sun', value: '#ffd166' },
+  { name: 'Peach', value: '#ff9f80' },
+  { name: 'Grape', value: '#b06cff' },
+  { name: 'Teal', value: '#2ec4b6' },
+  { name: 'Sand', value: '#f4d06f' },
+  { name: 'Midnight', value: '#243b6b' },
+  { name: 'Lilac', value: '#c5a3ff' },
+  { name: 'Aqua', value: '#4de1ff' },
+  { name: 'Berry', value: '#ff4d8d' },
+  { name: 'Plum', value: '#6d3c8d' },
+  { name: 'Forest', value: '#1b8a5a' },
+  { name: 'Olive', value: '#9bbf5a' },
+  { name: 'Charcoal', value: '#1c2b3a' },
+  { name: 'Snow', value: '#f7fbff' },
 ]
 
 export const MIN_NAME = 2
